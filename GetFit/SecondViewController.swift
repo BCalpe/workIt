352776bd59ff1adee4d3527d8f202ca22f83dd8a
Broderick Calpe <broderick.calpe@gmail.com>
2018-03-04 17:06:15 +0100

    Init project

---
+++ GetFit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GetFit
+//
+//  Created by Broderick Calpe on 2018-02-28.
+//  Copyright © 2018 Broderick Calpe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
